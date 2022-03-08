8776d0b3c3332d1e0e9b477dbed740d1e1623542
EviKoutro <46053162+EviKoutro@users.noreply.github.com>
2022-03-08 12:09:22 +0200

    Add files via upload

---
+++ app/SongSelectionActivity.java
@@ -0,0 +1,477 @@
+package com.example.project_katanemimena_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class SongSelectionActivity extends AppCompatActivity implements MyItemClickListener{
+
+    private RecyclerView recyclerView;
+    private LinearLayoutManager layoutManager;
+    private MyAdapter mAdapter;
+    private ArrayList<String> songs;
+    private int brokerPort;
+    private String[] songRequest = new String[2];
+    private String serverIP ="192.168.1.2";
+    private static int[] brokerPorts = {8900,8901,8902};
+    private MediaPlayer mediaPlayer;
+    //Used to pause/resume MediaPlayer
+    private int resumePosition;
+    private boolean donePlaying;
+
+    private ProgressDialog dialogSongs;
+    private ProgressDialog dialogData;
+
+    private getSongs asyncTaskGetSong;
+    private getSongData asyncTaskGetData;
+
+    private ImageButton ib_play_pause;
+
+    private boolean downloadSong;
+    private ArrayList<String> downloadedSongs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song);
+
+        songs = new ArrayList<>();
+
+        downloadedSongs = new ArrayList<>();
+        downloadSong=false;
+
+        Intent intent = getIntent();
+
+        recyclerView = (RecyclerView) findViewById(R.id.song_recycler_view);
+        recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL)); //add divider
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter
+        mAdapter = new MyAdapter(songs);
+        mAdapter.setOnItemClickListener(this);
+        recyclerView.setAdapter(mAdapter);
+
+        songRequest[0] = intent.getStringExtra("artist");
+        songRequest[1] = "all_songs";
+
+        initMediaPlayer();
+
+        dialogSongs = new ProgressDialog(SongSelectionActivity.this);
+
+        ib_play_pause = (ImageButton) findViewById(R.id.play_pause_button);
+        ib_play_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer.isPlaying()){
+                    pauseMedia();
+                    ib_play_pause.setImageResource(R.drawable.play_arrow);
+                }
+                else if(donePlaying){
+                    downloadSong=false;
+                    new getSongData().execute(songRequest);
+                    ib_play_pause.setImageResource(R.drawable.pause);
+                }
+                else{
+                    resumeMedia();
+                    ib_play_pause.setImageResource(R.drawable.pause);
+                }
+            }
+        });
+
+        final ImageButton ib_stop = (ImageButton) findViewById(R.id.stop_button);
+        ib_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ib_play_pause.setImageResource(R.drawable.play_arrow);
+                stopMedia();
+            }
+        });
+
+        final ImageButton ib_download = (ImageButton) findViewById(R.id.download_button);
+        ib_download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                downloadSong=true;
+                new getSongData().execute(songRequest);
+            }
+        });
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                donePlaying=true;
+                ib_play_pause.setImageResource(R.drawable.play_arrow);
+            }
+
+        });
+
+        asyncTaskGetSong = new getSongs();
+        asyncTaskGetSong.execute(songRequest);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        String song = songs.get(position);
+        if(song != null){
+            //Toast.makeText(this,"SONG "+song, Toast.LENGTH_SHORT).show();
+            songRequest[1] = song;
+            ib_play_pause.setImageResource(R.drawable.play_arrow);
+        }
+    }
+
+    private class getSongs extends AsyncTask<String[], Integer, ArrayList<String>> {
+        private String[] songRequestAsync;
+        private Socket requestSocket;
+        private int code,threadPort;
+        ArrayList<String> songStrings;
+        private CountDownTimer countDownTimer;
+
+        @Override
+        protected void onPreExecute() {
+            dialogSongs.setMessage("Getting songs, please wait.");
+            dialogSongs.show();
+            countDownTimer = new CountDownTimer(10000, 1000) {
+
+                public void onTick(long millisUntilFinished) {
+
+                }
+
+                public void onFinish() {
+                    asyncTaskGetSong.cancel(true);
+                    if(dialogSongs.isShowing())
+                        dialogSongs.cancel();
+                    new AlertDialog.Builder(SongSelectionActivity.this)
+                            .setTitle("Error")
+                            .setMessage("Error retrieving songs.\nDo you want to try again?")
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    asyncTaskGetSong = new getSongs();
+                                    asyncTaskGetSong.execute(songRequestAsync);
+                                }
+                            })
+                            .setNegativeButton("NO", null)
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+
+                }
+            }.start();
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+
+        }
+
+        protected ArrayList<String> doInBackground(String[]... songRequests) {
+            songRequestAsync = songRequests[0];
+            requestSocket = new Socket();
+            songStrings = new ArrayList<>();
+            //we reach out to a random broker
+            Random r = new Random();
+            brokerPort = brokerPorts[r.nextInt(3)];
+            //connect to the broker
+            try {
+                connectAndSendRequest();
+                requestSocket.close();
+
+                Log.d("LOG CAT:","Artist 1 : " + songStrings.get(0));
+
+
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            return songStrings;
+        }
+
+        private void connectAndSendRequest() throws IOException, ClassNotFoundException {
+            requestSocket = new Socket(serverIP, brokerPort);
+            Log.d("LOG CAT:","CONNECTED TO BROKER");
+            InputStream inputStream = requestSocket.getInputStream();
+            DataInputStream dataInputStream = new DataInputStream(inputStream);
+            // get the port of the broker thread that handle our request
+            threadPort = dataInputStream.readInt();
+            dataInputStream.close();
+            requestSocket.close();
+            //connect with the thread
+            requestSocket = new Socket(serverIP, threadPort);
+            OutputStream outputStream = requestSocket.getOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+            //send the request
+            objectOutputStream.writeObject(songRequestAsync);
+            objectOutputStream.flush(); // send the message
+            inputStream = requestSocket.getInputStream();
+            dataInputStream = new DataInputStream(inputStream);
+            code =  dataInputStream.readInt(); //read code
+            if(code == threadPort){
+                inputStream = requestSocket.getInputStream();
+                ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+                songStrings.addAll((ArrayList<String>) objectInputStream.readObject());
+            }
+            else{
+                requestSocket.close();
+                brokerPort = code;
+                connectAndSendRequest();
+            }
+        }
+
+        protected void onPostExecute(ArrayList<String> result) {
+            dialogSongs.dismiss();
+            dialogSongs=null;
+            countDownTimer.cancel();
+            mAdapter.notifyDataSetChanged(result);
+        }
+    }
+
+    private class getSongData extends AsyncTask<String[], Integer, byte[]> {
+        private String[] songRequestAsync;
+        private Socket requestSocket;
+        private int code,threadPort;
+        byte[] songData;
+        CountDownTimer countDownTimer;
+
+        protected void onProgressUpdate(Integer... progress) {
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+            dialogData = new ProgressDialog(SongSelectionActivity.this);
+            dialogData.setMessage("Getting song data, please wait.");
+            dialogData.show();
+            countDownTimer = new CountDownTimer(10000, 1000) {
+
+                public void onTick(long millisUntilFinished) {
+
+                }
+
+                public void onFinish() {
+                    asyncTaskGetData.cancel(true);
+                    if(dialogData.isShowing())
+                        dialogData.cancel();
+                    new AlertDialog.Builder(SongSelectionActivity.this)
+                            .setTitle("Error")
+                            .setMessage("Error retrieving songData.\nDo you want to try again?")
+                            .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    asyncTaskGetData = new getSongData();
+                                    asyncTaskGetData.execute(songRequestAsync);
+                                }
+                            })
+                            .setNegativeButton("NO", null)
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+
+                }
+            }.start();
+        }
+
+        protected byte[] doInBackground(String[]... songRequests) {
+            songRequestAsync = songRequests[0];
+            boolean downloadBool = checkDownload(songRequestAsync[1]);
+            try {
+                if(!downloadBool){
+                    requestSocket = new Socket();
+                    connectAndSendRequest();
+                    requestSocket.close();
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            return songData;
+        }
+
+        private byte[] connectAndSendRequest() throws IOException, ClassNotFoundException {
+            requestSocket = new Socket(serverIP, brokerPort);
+            Log.d("LOG CAT:","CONNECTED TO BROKER");
+            InputStream inputStream = requestSocket.getInputStream();
+            DataInputStream dataInputStream = new DataInputStream(inputStream);
+            // get the port of the broker thread that handle our request
+            threadPort = dataInputStream.readInt();
+            dataInputStream.close();
+            requestSocket.close();
+            //connect with the thread
+            requestSocket = new Socket(serverIP, threadPort);
+            OutputStream outputStream = requestSocket.getOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+            //send the request
+            objectOutputStream.writeObject(songRequestAsync);
+            objectOutputStream.flush(); // send the message
+            inputStream = requestSocket.getInputStream();
+            dataInputStream = new DataInputStream(inputStream);
+            code =  dataInputStream.readInt(); //read code
+            if(code == threadPort){
+                int packets = dataInputStream.readInt();
+                songData = new byte[packets*512];
+
+                Log.d("LOG CAT:","PACKETS : " + packets);
+                ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+                byte[] chunk = new byte[512];
+                int i=1;
+                //we need to reassemple the array with the chucks to get the full song
+                do{
+                    Log.d("LOG CAT:","TIMES : " + i);
+                    chunk = (byte[])objectInputStream.readObject();
+                    System.arraycopy(chunk,0, songData,(i-1)*512,512);
+                    i++;
+                } while(i<packets);
+                Log.d("LOG CAT:","TIMES : " + i);
+                requestSocket.close();
+            }
+            else{
+                requestSocket.close();
+                brokerPort = code;
+                connectAndSendRequest();
+                return null;
+            }
+            return songData;
+        }
+
+        protected void onPostExecute(byte[] result) {
+            dialogData.dismiss();
+            countDownTimer.cancel();
+            if(downloadSong)
+                saveSong(result,songRequestAsync[1]);
+            else
+                playMp3(result,songRequestAsync[1]);
+        }
+
+        private void saveSong(byte[] result,String filename) {
+/*            File path = new File(getApplicationContext().getFilesDir(),
+                    "/DownloadMP3s");
+            path.mkdirs();*/
+            File destination = new File(getApplicationContext().getFilesDir(),
+                    filename + ".mp3");
+            try {
+                destination.createNewFile();
+                FileOutputStream fos = new FileOutputStream(destination);
+                fos.write(result);
+                fos.close();
+                downloadedSongs.add(filename);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private boolean checkDownload(String song){
+            for(String downloadedSong:downloadedSongs){
+                if(downloadedSong.equals(song))
+                    return true;
+            }
+            return false;
+        }
+
+        private void playMp3(byte[] mp3SoundByteArray,String filename) {
+            File file = null;
+            if(checkDownload(filename)){
+                file = new File(getApplicationContext().getFilesDir(),
+                        filename + ".mp3");
+
+            }
+            else {
+                try {
+                    // create temp file that will hold byte array
+                    file = File.createTempFile("temp", "mp3", getCacheDir());
+                    file.deleteOnExit();
+                    FileOutputStream fos = new FileOutputStream(file);
+                    fos.write(mp3SoundByteArray);
+                    fos.close();
+                } catch (IOException ex) {
+                    String s = ex.toString();
+                    ex.printStackTrace();
+                }
+            }
+
+            try {
+                mediaPlayer.reset();
+                FileInputStream fis = new FileInputStream(file);
+                mediaPlayer.setDataSource(fis.getFD());
+                mediaPlayer.prepare();
+                playMedia();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+    //mediaPlayer
+
+    private void initMediaPlayer() {
+        mediaPlayer = new MediaPlayer();
+        donePlaying=true;
+    }
+
+    private void playMedia() {
+        donePlaying=false;
+        if (!mediaPlayer.isPlaying()) {
+            mediaPlayer.start();
+        }
+    }
+
+    private void stopMedia() {
+        donePlaying=true;
+        if (mediaPlayer == null) return;
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+        }
+    }
+
+    private void pauseMedia() {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+            resumePosition = mediaPlayer.getCurrentPosition();
+        }
+    }
+
+    private void resumeMedia() {
+        if (!mediaPlayer.isPlaying()) {
+            mediaPlayer.seekTo(resumePosition);
+            mediaPlayer.start();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopMedia();
+        mediaPlayer.release();
+        mediaPlayer = null;
+        super.onDestroy();
+    }
+}
